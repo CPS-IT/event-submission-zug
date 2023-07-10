f340c9958cba22b5a5129ca53468e3b4e406a84e
Vladimir Falcon <v.falcon@familie-redlich.de>
2023-07-10 17:54:19 +0200

    Update extension file structure

---
+++ Configuration/Icons.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'event-submition-job' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:event_submission/Resources/Public/Icons/event-submition-job.svg',
+    ]
+];
